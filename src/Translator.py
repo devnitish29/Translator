545a1307f3b246e79bc0cbe3232b215afb15fd52
devnitish29 <nitishkumar.singh@tarento.com>
2017-11-27 01:03:59 +0530

    -	Add ios support

--- src/Translator.py
+++ src/Translator.py
@@ -2,18 +2,25 @@ import os
 
 from googletrans import Translator
 
-translator = Translator()
-originalFile = input("Enter the file path :")
-language = input("Enter destination language code :")
-fileDirectory = input("Enter the directory path to store the translated file :")
-print("Relax a bit ....let me do the translation !!!")
-directory = fileDirectory + '/values-' + language;
-if not os.path.exists(directory):
-    os.makedirs(directory)
-newFilePath = directory + '/strings.xml'
-f = open(newFilePath, 'w')
-with open(originalFile) as fp:
-    with open(originalFile) as fp:
+
+def translate(text, language):
+    translator = Translator()
+    Translator(service_urls=[
+        'translate.google.com',
+        'translate.google.co.in',
+    ])
+    translatedText = translator.translate(text, dest=language)
+    return translatedText.text
+
+
+def translateAndroid(filepath, destinationLanguage, fileDirectory):
+    fileDirectory = fileDirectory + '/values-' + destinationLanguage
+    if not os.path.exists(fileDirectory):
+        os.makedirs(fileDirectory)
+
+    androidFilePath = fileDirectory + '/strings.xml'
+    f = open(androidFilePath, 'w')
+    with open(filepath) as fp:
         line = fp.readline()
         cnt = 1
         while line:
@@ -21,12 +28,46 @@ with open(originalFile) as fp:
             startInt = originalText.find('">') + 2
             endInt = originalText.find('</')
             oldText = originalText[startInt:endInt]
-            translatedText = translator.translate(oldText, dest=language)
-            newText = translatedText.text
+            newText = translate(oldText, destinationLanguage)
             changedText = originalText.replace(oldText, newText)
             f.write('\n' + changedText)
             line = fp.readline()
             cnt += 1
 
-f.close()
+    f.close()
+
+
+def translateIOS(filePath, destinationLanguage, fileDirectory):
+    if not os.path.exists(fileDirectory):
+        os.makedirs(fileDirectory)
+
+    iosFilePath = fileDirectory + '/Localizable.strings'
+    f = open(iosFilePath, 'w')
+    with open(filePath) as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            originalText = line.strip()
+            startInt = originalText.find('=')
+            endInt = originalText.find(';')
+            oldText = originalText[startInt:endInt]
+            newText = translate(oldText, destinationLanguage)
+            changedText = originalText.replace(oldText, newText)
+            f.write('\n' + changedText)
+            line = fp.readline()
+            cnt += 1
+    f.close()
+
+
+originalFile = input("Enter the file path :")
+destLanguage = input("Enter destination language code :")
+directory = input("Enter the directory path to store the translated file :")
+device = input("android or ios ?")
+
+print("Relax a bit ....let me do the translation !!!")
+if device.__eq__('android'):
+    translateAndroid(originalFile, destLanguage, directory)
+elif device.__eq__('ios'):
+    translateIOS(originalFile, destLanguage, directory)
+
 print("Hey WakeUp , Ur assigned job is done  !!!")
